34bb704189375591adb5600c6b0daffed440b84b
artemta1 <artmetai@outlook.com>
2023-07-14 17:58:08 +0300

    Version 1.0

---
+++ CODE.C
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main() {
+    int days, car, action;
+    float total;
+    char carList[5][10] = {"BMW", "Audi", "Mercedes", "Toyota", "Honda"};
+    float carCost[5] = {2000, 1800, 2200, 1200, 1000};
+    char selectedCar[10] = "";
+    while (1) {
+        
+        printf("Choose an action:\n");
+        printf("1. Select a car\n");
+        printf("2. Show selected car\n");
+        printf("3. Exit\n");
+        action = getch() - '0';
+        system("cls||clear");
+        if (action == 1) {
+            printf("Select a car:\n");
+            for (int i = 0; i < 5; i++) {
+                printf("%d. %s (%.2f Grivnas/day)\n", i+1, carList[i], carCost[i]);
+            }
+            car = getch() - '0';
+            strcpy(selectedCar, carList[car-1]);
+            system("cls||clear");
+            printf("You have selected: %s\n", selectedCar);
+            printf("Enter the number of rental days: ");
+            scanf("%d", &days);
+            system("cls||clear");
+            total = days * carCost[car-1];
+            system("cls||clear");
+            printf("Total rental cost: %.2f Grivnas\n", total);
+        } else if (action == 2) {
+            if (strlen(selectedCar) == 0) {
+                printf("You don't have any cars, would you like to rent one?\n");
+            } else {
+                printf("Your selected car: %s\n", selectedCar);
+            }
+        } else if (action == 3) {
+            printf("Goodbye!\n");
+            break;
+        }
+        getchar();
+        getchar();
+    }
+    return 0;
+}
